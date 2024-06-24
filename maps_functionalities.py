e5d87493047349dd6b9d0b6949d584d83c0522d4
Anke Hao <ankehao10@gmail.com>
2024-06-23 21:25:15 -0700

    first iteration: streamlit setup, hooked up with gemini

---
+++ maps_functionalities.py
@@ -0,0 +1,96 @@
+import pandas as pd # pip install pandas
+from google_apis import create_service
+import geocoder
+import touchgrass
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+maps_key = os.getenv('GOOGLE_MAPS_API')
+
+
+def places_hub(places_type, query, budget, num_recs):
+    client_secret_file = 'secrets\client_secret_desktop.json'
+    API_NAME = 'places'
+    API_VERSION = 'v1'
+    SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+    service = create_service(client_secret_file, API_NAME, API_VERSION, SCOPES)
+    
+    coordinates = get_maps_coordinates()
+    try:
+        assert coordinates is not None, "coordinates weren't retrieved"
+        latitude, longitude = coordinates
+    except Exception as e:
+        print(e)
+    
+    if places_type == 'text_search':
+        request_body = text_search(query, budget, num_recs, latitude, longitude )
+        response = service.places().searchText(
+        body = request_body,
+        # no spaces in the fields parameter!!!
+        fields = 'places.displayName,places.primaryType,places.rating,places.userRatingCount,places.priceLevel'
+        ).execute()
+        
+    elif places_type == 'nearby_search':
+        request_body = 'pending'
+        
+    return response['places']
+        
+        
+def text_search(query, budget, num_recs, latitude, longitude):
+    request_body = {
+        'textQuery': query,
+        'maxResultCount': num_recs,
+        'locationBias': {
+            'circle': {
+                'center': {
+                    'latitude': latitude,
+                    'longitude': longitude,
+                },
+                'radius': 1000
+            }
+        },
+        'rankPreference': 'RELEVANCE',
+        'priceLevels': touchgrass.construct_budget(budget),
+    }
+    return request_body
+
+
+def nearby_search(query, budget, num_recs, latitude, longitude):
+    request_body = {
+        'includedTypes': touchgrass.get_food_type(query),
+        'maxResultCount': num_recs,
+        'locationRestriction': {
+            'circle': {
+                'center': {
+                    'latitude': latitude,
+                    'longitude': longitude,
+                },
+                'radius': 1000
+            }
+        },
+        'rankPreference': 'POPULARITY',
+    }
+    return request_body
+    
+
+# try geocoder
+def get_geocoder_coordinates():
+    g = geocoder.ip('me')#this function is used to find the current information using our IP Add
+    if g.latlng is not None: #g.latlng tells if the coordinates are found or not
+        return g.latlng
+    else:
+        return None
+
+# try maps (seems to be slightly more accurate)
+def get_maps_coordinates():
+    url = f'https://www.googleapis.com/geolocation/v1/geolocate?key={maps_key}'
+
+    coordinates = requests.post(url).json()
+    # return a tuple with latitude and longitude
+    return (coordinates['location']['lat'], coordinates['location']['lng'])
+
+
+
+# df = pd.json_normalize(places_list)
+# df.to_csv('places_results.csv', index=False)
---
+++ touchgrass.py
@@ -0,0 +1,179 @@
+import os
+import requests
+import streamlit as st
+import vertexai
+import json
+from google.maps import places_v1
+from vertexai.generative_models import (
+    GenerationConfig,
+    GenerativeModel,
+    HarmBlockThreshold,
+    HarmCategory,
+    Part,
+)
+
+credential_path = r'C:\Users\anke_\AppData\Roaming\gcloud\application_default_credentials.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+# PROJECT_ID = "celtic-list-427003-c7"  # Your Google Cloud Project ID
+PROJECT_ID = "rosy-hangout-424004-f7"  # Your Google Cloud Project ID
+LOCATION = "us-central1"  # Your Google Cloud Project Region
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+
+@st.cache_resource
+def load_models():
+    """
+    Load the generative models for text and multimodal generation.
+
+    Returns:
+        Tuple: A tuple containing the text model and multimodal model.
+    """
+    text_model_pro = GenerativeModel("gemini-1.5-flash")
+    multimodal_model_pro = GenerativeModel("gemini-1.5-flash")
+    return text_model_pro, multimodal_model_pro
+
+
+def get_gemini_pro_text_response(
+    model: GenerativeModel,
+    contents: str,
+    generation_config: GenerationConfig,
+    stream: bool = True,
+):
+    safety_settings = {
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    }
+
+    responses = model.generate_content(
+        prompt,
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+        stream=stream,
+    )
+
+    final_response = []
+    for response in responses:
+        try:
+            # st.write(response.text)
+            final_response.append(response.text)
+        except IndexError:
+            # st.write(response)
+            final_response.append("")
+            continue
+    return " ".join(final_response)
+
+
+def get_gemini_pro_vision_response(
+    model, prompt_list, generation_config={}, stream: bool = True
+):
+    generation_config = {"temperature": 0.1, "max_output_tokens": 2048}
+    responses = model.generate_content(
+        prompt_list, generation_config=generation_config, stream=stream
+    )
+    final_response = []
+    for response in responses:
+        try:
+            final_response.append(response.text)
+        except IndexError:
+            pass
+    return "".join(final_response)
+
+#  may or may not use this
+def construct_query(cuisine, vibe):
+    return vibe + cuisine + ('restaurant' if cuisine != "cafe" else '')
+    
+def construct_budget(budget):
+    price_dict = {'casual': 'PRICE_LEVEL_INEXPENSIVE', 
+                  'mid-range': 'PRICE_LEVEL_MODERATE', 
+                  'fine dining': ['PRICE_LEVEL_EXPENSIVE', 
+                                  'PRICE_LEVEL_VERY_EXPENSIVE']
+                }
+    return price_dict[budget]
+
+def get_food_type(cuisine):
+    if cuisine != "cafe":
+        mod_cuisine = cuisine.replace(' ', '_')
+        return [f'{mod_cuisine}_restaurant', 'restaurant']
+    else:
+        return ["cafe"]
+        
+
+
+        
+
+st.header("touchgrass", divider="rainbow")
+text_model_pro, multimodal_model_pro = load_models()
+
+tab1, tab2, tab3 = st.tabs(
+    ["Find Food", "Go Sightseeing", "Find Events"]
+)
+
+with tab1:
+    # st.write("Using Gemini 1.5 Flash")
+    st.subheader("Find Food")
+
+    # This will get replaced with get location
+    location = st.text_input(
+        "What city are you in? \n\n", key="location", value="sf"
+    )
+    cuisine = st.text_input(
+        "What kind of food are you in the mood for? \n\n", key="cuisine", value="japanese"
+    )
+    budget = st.multiselect(
+        "What's your budget? \n\n",
+        [
+            "casual",
+            "mid-range",
+            "fine dining",
+        ],
+        key="budget",
+        default=["mid-range"],
+    )
+    vibe = st.multiselect(
+        "What kind of vibe are you looking for? \n\n",
+        [
+            "trendy",
+            "lively",
+            "romantic",
+            "casual",
+        ],
+        key="vibe",
+        default=["trendy"],
+    )
+    num_recs = st.slider(
+        "How many recommendations are you looking for? \n\n",
+        1, 5, 3
+    )
+
+    prompt = f"""You are a helpful local guide who knows the best restaurants in the area. \n
+    Your goal is to recommend great restaurants based on the requirements of the user. \n
+    cuisine: {cuisine} \n
+    location: {location} \n
+    budget: {budget} \n
+    vibe: {vibe} \n
+    how many recommendations: {num_recs}
+    """
+    config = {
+        "temperature": 0.8,
+        "max_output_tokens": 2048,
+    }
+
+    generate_t2t = st.button("Find my Food", key="generate_t2t")
+    if generate_t2t and prompt:
+        # st.write(prompt)
+        with st.spinner("Generating delicious matches ..."):
+            first_tab1, first_tab2 = st.tabs(["Matches", "Prompt"])
+            with first_tab1:
+                response = get_gemini_pro_text_response(
+                    text_model_pro,
+                    prompt,
+                    generation_config=config,
+                )
+                if response:
+                    st.write("Your matches:")
+                    st.write(response)
+            with first_tab2:
+                st.text(prompt)

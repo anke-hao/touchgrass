bd4b0f24214e09b8dca7f5bc69b1d19ddd34dddf
Anke Hao <ankehao10@gmail.com>
2024-07-01 22:31:34 -0700

    troubleshooting geolocation

--- maps_functionalities.py
+++ maps_functionalities.py
@@ -31,60 +31,10 @@ def get_food_type(cuisine):
 # for nearby_search: pending
 # for place_details: the place_id for the specific place that the user is querying
 
-def places_hub(places_type, query, budget, num_recs):
-    client_secret_file = 'secrets\client_secret_desktop.json'
-    API_NAME = 'places'
-    API_VERSION = 'v1'
-    SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
-    service = create_service(client_secret_file, API_NAME, API_VERSION, SCOPES)
-    
-    coordinates = get_maps_coordinates()
-    try:
-        assert coordinates is not None, "coordinates weren't retrieved"
-        latitude, longitude = coordinates
-    except Exception as e:
-        print(e)
-    
-    if places_type == 'text_search':
-        request_body = text_search(query, budget, num_recs, latitude, longitude)
-        response = service.places().searchText(
-        body = request_body,
-        # no spaces in the fields parameter!!!
-        fields = 'places.name,places.displayName,places.primaryType,places.rating,' \
-            'places.userRatingCount,places.priceLevel,places.generativeSummary.overview'
-        ).execute()
-        return response['places']
-        
-    elif places_type == 'nearby_search':
-        request_body = 'pending'
-    
-    elif places_type == 'place_details':
-        response = service.places().get(
-            name = query,
-            fields = '*'
-        ).execute()
-    return response
-        
+
 # https://developers.google.com/maps/documentation/places/web-service/experimental/places-generative
 
     
-def text_search(query, budget, num_recs, latitude, longitude):
-    request_body = {
-        'textQuery': query,
-        'maxResultCount': num_recs,
-        'locationBias': {
-            'circle': {
-                'center': {
-                    'latitude': latitude,
-                    'longitude': longitude,
-                },
-                'radius': 1000
-            }
-        },
-        'rankPreference': 'RELEVANCE',
-        'priceLevels': construct_budget(budget),
-    }
-    return request_body
 
 def text_search_new(query, budget, num_recs):
     coordinates = get_maps_coordinates()
@@ -168,4 +118,5 @@ def get_maps_coordinates():
 
     coordinates = requests.post(url).json()
     # return a tuple with latitude and longitude
+    print(coordinates)
     return (coordinates['location']['lat'], coordinates['location']['lng'])

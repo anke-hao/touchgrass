8d2fc18def1f640ed3372a7d6d1e2ffa47d9d91b
Anke Hao <ankehao10@gmail.com>
2024-07-07 17:51:01 -0700

    added documentation, cleanup, and maps widget

--- maps_functionalities.py
+++ maps_functionalities.py
@@ -1,10 +1,24 @@
 import streamlit as st
 import requests
 import googlemaps
+
+
 maps_key = st.secrets["GOOGLE_MAPS_API"]
 gmaps = googlemaps.Client(key=maps_key)
 
+
 def construct_budget(budget):
+    """
+    Based on whether user selected "casual", "mid-range", or "fine dining"
+    as their budget choice, return the corresponding request parameter needed
+    for the Places API
+
+    Parameters:
+        budget (str): user-specified budget
+        
+    Returns:
+        price_dict[budget]: Dictionary value corresponding to 'budget' key
+    """
     price_dict = {'casual': 'PRICE_LEVEL_INEXPENSIVE', 
                   'mid-range': 'PRICE_LEVEL_MODERATE', 
                   'fine dining': ['PRICE_LEVEL_EXPENSIVE', 
@@ -13,33 +27,46 @@ def construct_budget(budget):
     return price_dict[budget]
 
 
-def get_food_type(cuisine):
-    if cuisine != "cafe":
-        mod_cuisine = cuisine.replace(' ', '_')
-        return [f'{mod_cuisine}_restaurant', 'restaurant']
-    else:
-        return ["cafe"]
+def get_distance(place_id, coordinates):
+    """
+    Given the coordinates of the specified user location and the Place ID of 
+    the recommended place, calculate the distance in miles between the two
 
-
-def get_distance(coordinates, place_id):
+    Parameters:
+        place_id (str): unique Place id
+        coordinates (Dict): user-specified coordinates (latitude and longitude)
+    
+    Returns:
+        price_dict[budget]: Dictionary value corresponding to 'budget' key
+    """
     processed_place_id = place_id.replace('places/', 'place_id:')
     try:
         lat = coordinates['lat']
         lng = coordinates['lng']
     except Exception as e:
         print(e)
-    result = gmaps.distance_matrix((lat, lng), processed_place_id, mode = 'driving', units = 'imperial')
+    result = gmaps.distance_matrix((lat, lng), processed_place_id, 
+                                    mode = 'driving', units = 'imperial')
     miles = result["rows"][0]["elements"][0]["distance"]["text"]
     return miles
 
 
 def autocomplete(query):
+    """
+    Given the user's search query for an address/place, suggest a list of 
+        autocompletions
+
+    Parameters:
+        query (str): user query
+    
+    Returns:
+        response.json(): JSON with list of autocomplete suggestions of places
+    """
     url = 'https://places.googleapis.com/v1/places:autocomplete'
 
-    # Define the headers
     headers = {
         'Content-Type': 'application/json',
-        'X-Goog-Api-Key': maps_key,  # Replace 'API_KEY' with your actual Google Places API key
+        'X-Goog-Api-Key': maps_key,  
     }
     
     request_body = {
@@ -50,26 +77,50 @@ def autocomplete(query):
 
 
 def geocoder(address):
+    """
+    Given an address, return the latitude and longitude coordinates
+    
+    Parameters: 
+        address (str): address of a Place
+
+    Returns:
+        coords: dictionary with 'lat' and 'lng' as keys
+    """
     geocode_result = gmaps.geocode(address)
-    return geocode_result[0]["geometry"]["location"]
+    coords = geocode_result[0]["geometry"]["location"]
+    return coords
     
     
 def text_search_new(query, budget, num_recs, coordinates):
+    """
+    Given the user's search query and their specified budget, number of 
+        recommendations to generate, and coordinates (either their own or a
+        specified location), search for recommendations nearby
+
+    Parameters: 
+        query (str): user query
+        budget (str): user-specified budget
+        num_recs (int): user-specified number of recommendations to generate
+        coordinates (Dict): user-specified coordinates (latitude and longitude)
+            
+    Returns:
+        places: list of places, each with the information specified in the 
+        FieldMask of the headers
+    """
     try:
         lat = coordinates['lat']
         lng = coordinates['lng']
-        print(lat)
     except Exception as e:
         print(e)
     url = 'https://places.googleapis.com/v1/places:searchText'
 
-    # Define the headers
     headers = {
         'Content-Type': 'application/json',
         'X-Goog-Api-Key': st.secrets["GOOGLE_MAPS_API"],  
-        'X-Goog-FieldMask': 'places.name,places.displayName,places.primaryType,places.rating,' \
-            'places.userRatingCount,places.priceLevel,places.generativeSummary.overview,' \
-            'places.googleMapsUri'
+        'X-Goog-FieldMask': 'places.name,places.displayName,places.rating,' \
+                    'places.userRatingCount,places.priceLevel,' \
+                    'places.generativeSummary.overview,places.googleMapsUri,' \
+                    'places.formattedAddress'
     }
     
     request_body = {
@@ -88,29 +139,25 @@ def text_search_new(query, budget, num_recs, coordinates):
         'priceLevels': construct_budget(budget),
     }
     response = requests.post(url, headers=headers, json=request_body)
-    print(response.json())
-    return response.json()['places']
+    places = response.json()['places']
+    return places
 
 
-def get_place_details(name):
-    response = requests.get(f"https://places.googleapis.com/v1/{name}?fields=*&key={st.secrets['GOOGLE_MAPS_API']}")
+def get_place_details(place_id):
+    """
+    Given the (non-human-readable) name of a Place, get all the information 
+        possible from the Places API
+
+    Parameters: 
+        place_id (str): unique Place id
+        
+    Returns:
+        response.json(): JSON-formatted information about a Place. 
+            Documentation of data fields located here: 
+            https://developers.google.com/maps/documentation/places/web-service/data-fields
+    """
+    response = requests.get(
+        f"https://places.googleapis.com/v1/{place_id}?fields=*&key={st.secrets['GOOGLE_MAPS_API']}"
+        )
     return response.json()
 
-
-def nearby_search(query, budget, num_recs, latitude, longitude):
-    request_body = {
-        'includedTypes': get_food_type(query),
-        'maxResultCount': num_recs,
-        'locationRestriction': {
-            'circle': {
-                'center': {
-                    'latitude': latitude,
-                    'longitude': longitude,
-                },
-                'radius': 1000
-            }
-        },
-        'rankPreference': 'POPULARITY',
-    }
-    return request_body
-    
--- touchgrass.py
+++ touchgrass.py
@@ -1,6 +1,8 @@
 import streamlit_js_eval
 import streamlit as st
-import maps_functionalities
+import folium
+from streamlit_folium import st_folium
+import maps_functionalities as maps_func
 import google.generativeai as genai
 
 
@@ -10,79 +12,168 @@ initial_coordinates = streamlit_js_eval.get_geolocation()
 
 def load_model():
     """
-    Load the generative models for text and multimodal generation.
+    Load the generative model.
 
+    Parameters: N/A
+    
     Returns:
-        Tuple: A tuple containing the text model and multimodal model.
+        model_flash (GenerativeModel): Gemini 1.5 Flash
     """
     config = {
         "temperature": 0.7,
         "max_output_tokens": 2048,
     }
-    system_instruction = f"""You are a helpful local guide who knows the best food in the area. \n
-    You must answer questions about {st.session_state.place_name} as accurately as possible, given 
-    the user query {query} and the following JSON of details: \n
-    {st.session_state.place_details}
+    system_instruction = f"""
+        You are a helpful local guide who knows the best food in the area. \n
+        You must answer questions about {st.session_state.place_name} as 
+        accurately as possible, given the user query {query} and the following 
+        JSON of details: \n
+        {st.session_state.place_details}
     """
-    print(system_instruction)
     model_flash = genai.GenerativeModel(
         model_name = "gemini-1.5-flash",
         generation_config=config,
-        # safety_settings=safety_settings,
         system_instruction=system_instruction,
         )
     return model_flash
 
 
 def stream_data(query):
+    """
+    Preprocesses the response to be streamed
+
+    Parameters:
+        query (str): the user query to the chatbot
+    
+    Returns:
+        N/A (yields the response text chunks as they are generated)
+    """
     stream = st.session_state.chat.send_message(query, stream=True)
     for part in stream:
         yield part.text
        
         
 def get_llm_response(query):
+    """
+    Streams the LLM response
+
+    Parameters:
+        query (str): the user query to the chatbot
+    
+    Returns:
+        response (str): the generated response to the user query
+    """
     response = st.write_stream(stream_data(query))
     return response
 
 
 def get_place(place_id, place_name):
+    """
+    Stores key information about the specified Place and starts the Q&A session
+
+    Parameters:
+        place_id (str): the unique Place ID of the Place
+        place_name (str): the human-readable name of the Place
+    
+    Returns:
+        N/A 
+    """
     st.session_state.place = place_id
     st.session_state.place_name = place_name
-    st.session_state.place_details = maps_functionalities.get_place_details(st.session_state.place)
+    st.session_state.place_details = maps_func.get_place_details(st.session_state.place)
     st.session_state.messages = []
-    print(st.session_state.place_name)
     restaurant_start_chat()
     
 
 def restaurant_start_chat():
+    """
+    Preprocesses the response to be streamed, by yielding the response text 
+        chunks as they are generated
+
+    Parameters:
+        query (str): the user query to the chatbot
+    
+    Returns:
+        N/A 
+    """
     st.session_state.model = load_model()
     st.session_state.chat = st.session_state.model.start_chat(
         history=[],
     )
     
-    print("STARTED CHAT")
-    
 
-def display_choices(responses):
-    cols = st.columns(len(responses))
+def display_choices(recommendations):
+    """
+    Displays the recommendations of places based on the user 
+        specifications, dynamically changing based on the number of 
+        recommendations requested. The displayed information includes:
+        - Name
+        - Rating (and # of ratings)
+        - Distance
+        - Google Maps URI
+        - Generative summary overview
+
+    Parameters:
+        recommendations (str): the recommendations that were returned 
+    
+    Returns:
+        N/A 
+    """
+    cols = st.columns(len(recommendations))
     for i, col in enumerate(cols):
         with col:
-            response = responses[i]
-            st.write(f"**{response['displayName']['text']}**")
-            st.write(f"Rating: {response['rating']} ({response['userRatingCount']} reviews)")
-            distance = maps_functionalities.get_distance(st.session_state.coordinates, response['name'])
+            rec = recommendations[i]
+            st.write(f"**{rec['displayName']['text']}**")
+            st.write(
+                f"Rating: {rec['rating']} ({rec['userRatingCount']} reviews)")
+            distance = maps_func.get_distance(
+                rec['name'], st.session_state.coordinates)
             st.write(f"Distance: {distance}")
-            st.write("[Take Me to Google Maps](%s)" % response['googleMapsUri'])
-            if 'generativeSummary' in response:
-                st.write(f"About: {response['generativeSummary']['overview']['text']}")
+            st.write("[Take Me to Google Maps](%s)" % rec['googleMapsUri'])
+            if 'generativeSummary' in rec:
+                st.write(
+                    f"About: {rec['generativeSummary']['overview']['text']}")
             else:
-                st.write("Haven't got a summary for this one. Must be good though!")
-            st.button("Learn More", key=response['name'], on_click=get_place, args=(response['name'], response['displayName']['text']))
+                st.write(
+                    "Haven't got a summary for this one. Must be good though!")
+            st.button("Learn More", key=rec['name'], on_click=get_place, 
+                      args=(rec['name'], rec['displayName']['text']))
+    display_map(recommendations)
 
 
+def display_map(recommendations):
+    lat, lng = (st.session_state.coordinates['lat'], st.session_state.coordinates['lng'])
+    # lat = st.session_state.coordinates['lat']
+    m = folium.Map(location=[lat, lng], zoom_start=12)
+    folium.Marker(
+            [lat, lng], popup="Your Location", tooltip="Your Location", icon=folium.Icon(color='red')
+    ).add_to(m)
+    for rec in recommendations:
+        rec_coords = maps_func.geocoder(rec['formattedAddress'])
+        rec_name = rec['displayName']['text']
+        folium.Marker(
+            [rec_coords['lat'], rec_coords['lng']], popup=rec_name, tooltip=rec_name, icon=folium.Icon()
+        ).add_to(m)
+
+    # call to render Folium map in Streamlit
+    return st_folium(m, width='100%')
+    
+    
 def display_autocomplete_options(search_term):
+    """
+    Displays the suggestions for autocomplete based on user input
+
+    Parameters:
+        search_term: the user query to search for a place
+    
+    Returns:
+        parsed_suggestions (list): a list of suggested places based on the 
+        Places API Autocomplete functionality 
+        OR
+        None if there is no user query inputted
+    """
     if search_term:
-        suggestions = maps_functionalities.autocomplete(search_term)["suggestions"]
+        suggestions = maps_func.autocomplete(search_term)["suggestions"]
         parsed_suggestions = []
         for suggestion in suggestions:
             display_name = suggestion['placePrediction']['text']['text']
@@ -92,8 +183,16 @@ def display_autocomplete_options(search_term):
         return None
 
 
-# input: json of restaurant details
-def restaurant_qa(query):            
+def restaurant_qa(query):
+    """
+    Displays and handles the Q&A restaurant chatbot experience
+
+    Parameters:
+        query (str): the user query to the chatbot
+    
+    Returns:
+        N/A
+    """            
     st.session_state.messages.append({"role": "user", "content": query})
     with st.chat_message("user"):
         st.markdown(query)
@@ -103,30 +202,25 @@ def restaurant_qa(query):
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
+# Initializing session state variables if they were not already initialized
 if 'food_options' not in st.session_state:
     st.session_state.food_options = False
-    
 if 'place' not in st.session_state:
     st.session_state.place = False
     
-            
+    
+# Rendering the headers of the website            
 st.header("touchgrass", divider="rainbow")
-
 st.subheader("Find Food")
 
-
-# Limitation: Within a form, the only widget that can have a callback function 
-# is st.form_submit_button, which means I'm going the error route instead of simply
-# disabling the button until the text field is filled out
-# Limitation: custom module st_searchbox does not work within a streamlit form
-
+# Display form for user input
 with st.form("my-form"):
-    query = st.text_input(
+    query = st.text_input( # text input for main user query
         "What kind of food are you in the mood for? \n\n", 
         placeholder="e.g. sushi, tacos, burgers",
     )
 
-    budget = st.radio(
+    budget = st.radio( # radio button for user budget
         "What's your budget? \n\n",
         [
             "casual",
@@ -136,65 +230,81 @@ with st.form("my-form"):
         key="budget",
     )
 
-    num_recs = st.slider(
+    num_recs = st.slider( # slider for number of recommendations to generate
         "How many recommendations are you looking for? \n\n",
         1, 5, 3,
         key="num_recs"
     )
-    # Limitation: on_change for selectbox is only when you actually click on an 
-    # option and it changes, not if you type in something. Used custom component instead
 
-    location = st.text_input(
-        label = "Optional: where do you want to search around, if not your current location? \n\n",
+    location = st.text_input( # optional text input for user to search 
+        label = "Optional: where do you want to search around, " \
+                "if not your current location? \n\n",
         placeholder="location to search around",
     )
+    
     selections = []
     final_location = None
-    if location:
+    if location: # if user filled out a specified location, autocomplete the addr
         selections = display_autocomplete_options(location)
         final_location = st.selectbox(
             label = "Options \n\n",
             placeholder="location to search around",
             options = selections,
         )
+        
     submit_button = st.form_submit_button("Find my Food")
 
-        
+# if the user clicked submit without inputting what they wanted to search, 
+# throw an error to prompt them to input something and stop execution
 if submit_button and not query:
     st.error("Please specify what food you're craving!", icon="🚨")
     st.stop()
 
-if submit_button:
+# happy path: user inputted their search and clicked submit
+if submit_button and query:
     with st.spinner("Generating delicious matches ..."):
-        if final_location: # use the specified location instead of user's current location
-            st.session_state.coordinates = maps_functionalities.geocoder(final_location)
-            print(final_location)
-        elif initial_coordinates: # process and use the user's current location
-            st.session_state.coordinates = {"lat": initial_coordinates['coords']['latitude'], 
-                            "lng": initial_coordinates['coords']['longitude']}
-        st.session_state.food_options = maps_functionalities.text_search_new(
+        # if a location is specified, use that instead of user's current location
+        if final_location: 
+            st.session_state.coordinates = maps_func.geocoder(final_location)
+        # otherwise, process and use the user's current location
+        elif initial_coordinates: 
+            st.session_state.coordinates = {
+                "lat": initial_coordinates['coords']['latitude'], 
+                "lng": initial_coordinates['coords']['longitude']}
+        # get food recommendations
+        st.session_state.food_options = maps_func.text_search_new(
             query = query,
             budget = budget,
             num_recs = num_recs,
             coordinates = st.session_state.coordinates
         )
-        
+
+# if food recommendations were returned, display them
 if st.session_state.food_options:
     st.subheader("Your matches:", divider='gray')
     display_choices(st.session_state.food_options) 
     
+# if user clicked "Learn More" and additional information about the place was 
+# retrieved, start chat session about the place 
 if st.session_state.place:
     st.subheader(f"Chat about {st.session_state.place_name}", divider='gray')
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
     if st.session_state.messages == []:
-        starter_prompt = f"""Give the user a friendly introduction to {st.session_state.place_name}.\n
-        Make sure to let them know they can ask you about the hours, price range, or any other details."""
+        starter_prompt = f"""
+        Give the user a friendly introduction to 
+        {st.session_state.place_name}.\n
+        Make sure to let them know they can ask you about the hours, price 
+        range, or any other details.
+        """
         with st.chat_message("assistant"):
-            st.session_state.messages.append({"role": "assistant", "content": get_llm_response(starter_prompt)})
+            st.session_state.messages.append({
+                "role": "assistant", "content": get_llm_response(starter_prompt)
+                })
         
     # Accept user input
-    if query := st.chat_input(f"Ask me anything about {st.session_state.place_name}", key='user_chat'):
+    if query := st.chat_input(
+        f"Ask me anything about {st.session_state.place_name}", 
+        key='user_chat'):
         restaurant_qa(query)
-        
